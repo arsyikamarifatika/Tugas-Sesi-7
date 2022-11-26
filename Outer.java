7dbbb3f02a5e2f5031b557230803e37f15da4412
arsyikamarifatika <93310658+arsyikamarifatika@users.noreply.github.com>
2022-11-26 23:09:03 +0700

    Add files via upload

--- Outer.java
+++ Outer.java
@@ -1,13 +1,25 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+package Sesi7;
 
-/**
- *
- * @author arsyika
- */
 public class Outer {
+    int number=0;
     
+    void outerMethod(){
+        class Inner{
+        public void print(){
+            System.out.println("Mengakses inner class yang ke: " + (++number));
+        }
+    }
+        Inner in = new Inner();
+        in.print();
+    }
+}
+
+class OuterAccess{
+    
+    public static void main(String[] args) {
+        Outer out = new Outer();
+        out.outerMethod();
+        out.outerMethod();
+        out.outerMethod();
+    }
 }
